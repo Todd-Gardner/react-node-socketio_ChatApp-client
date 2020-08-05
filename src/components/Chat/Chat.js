da277465f675f8dac13768292720c911d3c133d3
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2020-08-05 12:14:25 -0400

    useEffect for users joining and disconnecting

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-import './Chat.css';
+import "./Chat.css";
 
 // TODO: Create the CSS or use bootstrap //
 
-const Chat = () => {
-  return <h1>Chat Page</h1>;
+let socket;
+
+const Chat = ({ location }) => {
+  const ENDPOINT = "localhost:5000";
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  useEffect(() => {
+    // Save query string to an object ie {Name: x, Room: y} -> destructure
+    const { name, room } = queryString.parse(location.search);
+
+    // Initialize socket listening on 'host:port' endpoint
+    socket = io(ENDPOINT);
+
+    // Save name + room from the URL query to state
+    setName(name);
+    setRoom(room);
+
+    // Send join{name:name, room:room} to the server (index.js)
+    socket.emit("join", { name, room }, () => { }); //Callback from server
+    
+    // onUnmount - Cleanup for useEffect. Calls disconnect from server
+    return () => {
+      socket.emit('disconnect');
+
+      // Close socket when used disconnects
+      socket.off();
+    }
+  }, [ENDPOINT, location.search]); //only reload page when theese change
+
+  return (
+    <div>
+      <h1>Chat Page</h1>
+      <h3>Welcome {name}!</h3>
+    </div>
+  );
 };
 
 export default Chat;
